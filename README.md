8d9b786983d7211e70d0aa6eab5e34c3a88c2bde
Sam <8408317+samgabrail@users.noreply.github.com>
2023-04-25 16:55:59 +0000

    update startup script and README

--- README.md
+++ README.md
@@ -7,3 +7,39 @@ Run the startup script:
 ```bash
 ./startup_script.sh
 ```
+
+## Testing
+
+To test logs and rotation, you can generate some secrets:
+
+```bash
+vault secrets enable kv
+for i in {1..50}
+  do
+    printf "."
+    vault kv put kv/$i-secret-50 id="${i+10}" >/dev/null 2>&1
+done
+```
+
+Create some tokens:
+
+```bash
+for i in {1..50}
+  do
+    printf "."
+    vault token create -policy=default >/dev/null 2>&1
+done
+```
+
+Attempt to login multiple times with errors:
+
+```bash
+for i in {1..50}
+  do
+    printf "."
+    vault login \
+      -method=userpass \
+      username=learner \
+      password=vtl-password >/dev/null 2>&1
+done
+```
--- startup_script.sh
+++ startup_script.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-
+Gre='\e[0;92m';
+RCol='\e[0m';
 ## Configure Vault
-
+echo -e "${Gre}✔ Script Started Running, Please be patient, this will take about a minute"
 ### Initialize and Unseal Vault
-
+echo -e "${Gre}✔ Running Vault Initialization and unseal"
+echo -e "${Gre}✔ Logs will show up in the file /workspaces/vault-monitoring/vault/logs/vault.log"
 mkdir -p /workspaces/vault-monitoring/vault/logs/
 touch /workspaces/vault-monitoring/vault/logs/vault.log
 vault server -config=/workspaces/vault-monitoring/vault/config/server.hcl > /workspaces/vault-monitoring/vault/logs/vault.log 2>&1 &
@@ -39,3 +41,9 @@ vault audit enable file file_path=/workspaces/vault-monitoring/vault/logs/vault-
 ## Start the Monitoring stack
 
 docker compose up -d
+
+echo -e "${Gre}✔ Script Completed"
+echo -e "${Gre}Run the following commands to get started:"
+echo -e "${RCol}export VAULT_ADDR=http://127.0.0.1:8200"
+echo 'export LEARN_VAULT=/tmp/learn-vault-monitoring'
+echo "vault login -no-print \$(grep 'Initial Root Token' \$LEARN_VAULT/.vault-init | awk '{print \$NF}')"
